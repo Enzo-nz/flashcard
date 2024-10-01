945e1d503b6683133d152d0c1df18b90d100ad46
Enzo-nz <enzo.camilo@escola.pr.gov.br>
2024-10-01 16:39:36 -0300

    01/10
    ........................................................

---
+++ assets/pergunta.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Progrmação',
+    'O que é python',
+    'O Python é uma linguagem dde peogramação',
+)
+
+criaCartao(
+    'Geografria',
+    'Qual é a capital da França?',
+    'Acapital da França é Paris',
+)
+
+criaCartao(
+    'Programaçao',
+    'O que é uma função',
+    'Uma função é um bloco de código que executa alguma tarefa',
+)

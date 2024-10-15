c8f212bdf77bb3338c8a222d34f69fe22eb2913d
Enzo-nz <enzo.camilo@escola.pr.gov.br>
2024-10-15 16:52:10 -0300

    15/10
    ;;;;;;;;;;;;;;;;

---
+++ pergunta.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Educação Financeira',
+    'O que é a sigla CDB?',
+    'Certifcado de Deposito Bancario',
+)
+
+criaCartao(
+    'Matematica',
+    'Qual a raiz quadrada e 81?',
+    'A raiz quadrada de 81 é 9',
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris',
+)
+
+criaCartao(
+    'Ingles',
+    'Como se fala"como voce vai" em ingles?',
+    'Se fala "How are you?"',
+)
